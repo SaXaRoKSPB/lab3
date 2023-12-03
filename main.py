22aa41e304e9eaf923ac2cfa5d273030fe2a40e2
Dima <panyukoff.dima@yandex.ru>
2023-12-03 22:51:52 +0300

    repair cod

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-from tkinter import *
+from tkinter import Label, Tk, PhotoImage, Entry, Button
 import random, string
-def clicked():
-    global counter           #Нужен как счётчик для количества раз нажатий на кнопку, чтобы я мог заменять надпись, при повторном нажатии
-    global title_key                #Нужен, чтобы заменять выведенную уже запись на новую (Ошибку на новый код например)
-    flag = 0                 #Нужен будет для проверки состоит ли строка только из заглавных букв (решил заморочиться и добавить проверку на маленькие и большие буквы) :)
+
+def new_key(text):
+    flag = 0
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    word = txt.get()
     first_block = ''
     second_block = ''
     thirty_block = ''
     key = ''
-    if word.isalpha() == True:         #Проверка на то, чтобы в строке были только буквы (да тоже решил заморочиться и сделать проверку на глупого пользователя) :)
+    if text.isalpha():         
         for i in range(3):
-            first_block += random.choice(word)
+            first_block += random.choice(text)
         for i in range(3):
-            thirty_block += random.choice(word)
+            thirty_block += random.choice(text)
         number_letters = []
-        for letter in range(len(word)):
-            index_letter = alphabet.index(word[letter]) + 1
-            if index_letter > 26:              #Поднимаю флаг если появилась маленькая буква
+        for letter in range(len(text)):
+            index_letter = alphabet.index(text[letter]) + 1
+            if index_letter > 26:              
                 flag = 1
             else:
                 number_letters.append(index_letter % 10)
@@ -26,32 +24,51 @@ def clicked():
             for i in range(6):
                 second_block += str(random.choice(number_letters))
         key = first_block + '-' + second_block + '-' + thirty_block
-    if counter == 0:
-        if word.isalpha() == True and len(word) == 6 and flag != 1:
-            title_key = Label(window, text = "Ваш новый ключ: " + key, background="gray19", foreground="white", font = ("Times New Roman", 30))
-            title_key.place(relx=0.02, rely=0.8)
+    return key, flag
+
+def clicked():  
+    global COUNTER
+    global TITLE_KEY                        
+    word = txt.get()
+    key, flag = new_key(word)
+    
+    if COUNTER == 0:
+        if word.isalpha() and len(word) == 6 and flag != 1:
+            TITLE_KEY = Label(window, text="Ваш новый ключ: " + key,
+                              background="gray19", foreground="white", font=("Times New Roman", 30))
+            TITLE_KEY.place(relx=0.02, rely=0.8)
         else:
-            title_key = Label(window, text = "ОШИБКА! Проверьте корректный ввод слова", background="gray19", foreground="red", font = ("Times New Roman", 30))
-            title_key.place(relx=0.02, rely=0.8)
+            TITLE_KEY = Label(window, text="ОШИБКА! Проверьте корректный ввод слова", 
+                              background="gray19", foreground="red", font=("Times New Roman", 30))
+            TITLE_KEY.place(relx=0.02, rely=0.8)
     else:
-        if word.isalpha() == True and len(word) == 6 and flag != 1:
-            title_key.configure(text="Ваш новый ключ: " + key, background="gray19", foreground="white", font = ("Times New Roman", 30))
+        if word.isalpha() and len(word) == 6 and flag != 1:
+            TITLE_KEY.configure(text="Ваш новый ключ: " + key, background="gray19", 
+                                foreground="white", font=("Times New Roman", 30))
         else:
-            title_key.configure(text="ОШИБКА! Проверьте корректный ввод слова", background="gray19", foreground="red", font = ("Times New Roman", 30))
-    counter += 1
-counter = 0
+            TITLE_KEY.configure(text="ОШИБКА! Проверьте корректный ввод слова", 
+                                background="gray19", foreground="red", font=("Times New Roman", 30))
+    COUNTER += 1
+
+COUNTER = 0
+
 window = Tk()
 window.title("Генератор ключа")
 window.geometry('1250x700')
-window.resizable (width=False, height=False)    #Чтобы нельзя было поменять размер экрана, т.к. поставил как надо по картинке
-image_ETS2 =  PhotoImage(file = 'image.png')
-window_image = Label(window, image = image_ETS2)
+window.resizable (width=False, height=False)
+
+image_ETS2 = PhotoImage(file='image.png')
+window_image = Label(window, image=image_ETS2)
 window_image.grid(row=0, column=0)
+
 txt = Entry(window, width=20, font=("Times New Roman", 12))  
 txt.place(relx=0.26, rely=0.15)
-txt.focus()    #Ставит курсор сразу на поле
-title_word = Label(window, text = "Введите 6-ти буквенное слово", background="midnightblue", foreground="white", font = ("Times New Roman", 30))
+txt.focus() 
+
+title_word = Label(window, text="Введите 6-ти буквенное слово", background="midnightblue", foreground="white", font=("Times New Roman", 30))
 title_word.place(relx=0.13, rely=0.05)
-button = Button(window, text="Enter", width=7, font = ("Times New Roman", 10), bg = "midnightblue", fg = "white", command = clicked)
+
+button = Button(window, text="Enter", width=7, font=("Times New Roman", 10), bg="midnightblue", fg="white", command=clicked)
 button.place(relx = 0.36, rely = 0.15)
-window.mainloop()     #Чтобы приложение не закрывалось, пока не дадут такую команду
+
+window.mainloop()     
